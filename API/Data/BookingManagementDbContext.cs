c67689f53714e3575689128a6517691f123b14e8
Fahri Hanif <fahrihanif03@gmail.com>
2023-09-29 09:48:52 +0700

    Add cardinality and relationships

--- API/Data/BookingManagementDbContext.cs
+++ API/Data/BookingManagementDbContext.cs
@@ -26,5 +26,23 @@ public class BookingManagementDbContext : DbContext
             e.Email,
             e.PhoneNumber
         }).IsUnique();
+        
+        // One University has many Educations
+        modelBuilder.Entity<University>()
+                    .HasMany(e => e.Educations)
+                    .WithOne(u => u.University)
+                    .HasForeignKey(e => e.UniversityGuid)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+        /*modelBuilder.Entity<Education>()
+                    .HasOne(u => u.University)
+                    .WithMany(e => e.Educations)
+                    .HasForeignKey(u => u.UniversityGuid);*/
+        
+        // One Education has one Employee
+        modelBuilder.Entity<Education>()
+                    .HasOne(e => e.Employee)
+                    .WithOne(e => e.Education)
+                    .HasForeignKey<Education>(e => e.Guid);
     }
 }
