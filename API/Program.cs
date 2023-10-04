1a7d6789ed4b87cf4f6e66a14b3aa4e562cf0be3
Fahri Hanif <fahrihanif03@gmail.com>
2023-10-04 11:26:39 +0700

    Add FluentValidation

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,14 @@
+using System.Net;
+using System.Reflection;
 using API.Contracts;
 using API.Data;
+using API.DTOs.Employees;
 using API.Repositories;
+using API.Utilities.Handlers;
+using API.Utilities.Validations.Employees;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,12 +19,35 @@ builder.Services.AddDbContext<BookingManagementDbContext>(option => option.UseSq
 
 // Add repositories to the container.
 builder.Services.AddScoped<IUniversityRepository, UniversityRepository>();
+builder.Services.AddScoped<IEducationRepository, EducationRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountRoleRepository, AccountRoleRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+       .ConfigureApiBehaviorOptions(options =>
+        {
+            // Custom validation response
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var errors = context.ModelState.Values
+                                    .SelectMany(v => v.Errors)
+                                    .Select(v => v.ErrorMessage);
+
+                return new BadRequestObjectResult(new ResponseValidatorHandler(errors));
+            };
+        });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add FluentValidation Services
+builder.Services.AddFluentValidationAutoValidation()
+       .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
